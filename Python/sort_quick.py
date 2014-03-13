00668fce1b2b52c3781d0f7c54b09f304d670ce4
Stephen Pryor <stephenpryor02@yahoo.com>
2014-03-12 23:25:11 -0700

    Updated quick sort function
    Update the quick sort function to use a separate partition function.

--- Python/sort_quick.py
+++ Python/sort_quick.py
@@ -3,17 +3,23 @@
 
 import random
 
-def sort_quick(lst):
-  if len(lst) < 1:
-    return lst
-  pivot_index = random.randrange(0, len(lst))
-  pivot_value = lst[pivot_index]
-  del lst[pivot_index]
-  below = []
-  above = []
-  for val in lst:
-    if val <= pivot_value:
-      below.append(val)
-    else:
-      above.append(val)
-  return sort_quick(below)+[pivot_value]+sort_quick(above)
+def partition(arr, p, r):
+  x = arr[r]
+  i = p - 1
+  for j in range(p, r):
+    if arr[j] <= x:
+      i = i + 1
+      arr[i], arr[j] = arr[j], arr[i]
+  arr[i + 1], arr[r] = arr[r], arr[i + 1]
+  return i + 1
+
+def randomPartition(arr, p, r):
+  i = random.randrange(p, r)
+  arr[r], arr[i] = arr[i], arr[r]
+  return partition(arr, p, r)
+
+def randomQuickSort(arr, p, r):
+  if p < r:
+    q = randomPartition(arr, p, r)
+    randomQuickSort(arr, p, q - 1)
+    randomQuickSort(arr, q + 1, r)
